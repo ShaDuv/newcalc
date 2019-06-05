dbe3d0faf221b3a06c9676b96947253607349ba9
Hugh Donkin and Shawna Duvall <hugh@>
2019-06-05 13:55:45 -0700

    add functional forms and button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 /* Basic Math Calculation */
 
-// Input
-var num1 = parseInt(prompt("Enter a number."));
-var num2 = parseInt(prompt("Enter another number."));
 
-/*This is the add function */
+/*Business Backend Logic*/
 
 var add = function (num1, num2){
 	return num1 + num2;
@@ -30,45 +27,69 @@ var divide = function (num1, num2){
 };
 
 /*This is us returning an answer to the user*/
-var result = divide (num1, num2);
+// var result = divide (num1, num2);
 
-alert (result);
+// Input
+$(document).ready(function() {
 
-/* BMI Calculation */
-var bmi = function (weight, height ) {
-  return (703 * weight) / (height * height);
-};
 
-var weight = parseFloat(prompt("Enter your weight in pounds:"));
-var height = parseFloat(prompt ("Enter your height in inches:"));
+  $("form#add").submit(function(event) {
+  event.preventDefault();
+  var num1 = parseInt($("#add1").val());
+  var num2 = parseInt($("#add2").val());
+  var result = add(num1, num2);
+  console.log(result);
+  $("#output").text(result);
 
-var result = bmi (weight, height).toFixed(1);
-alert ((result) + " This number actually means NOTHING");
+  });
 
-/*Converts Celsius to Fahrenheit*/
-var tempC = function (num1) {
-  return (num1 * 1.8) + 32;
-};
-var num1 = parseInt(prompt("Enter the temp in Celsius:"))
-var result = tempC (num1);
-alert ("That is " + (result) + " degrees Celsius.");
 
-/*Converts Fahrenheit to Celsius*/
-var tempF = function (num1) {
-  return (num1 - 32) * .5556;
-};
-var num1 = parseInt(prompt("Enter the temp in Fahrenheit:"));
-var result = tempF (num1);
-alert ("That is " + (result) + " degrees Fahrenheit.");
+});
 
-/*Converts Gallons to Liters*/
-var gallon = function (num1) {
-  return num1 * 3.78541;
-};
 
-var num1 = parseFloat (prompt("Enter in the number of gallons:"));
-var result = gallon (num1);
-alert ((num1) + " gallons is " + (result) + " liters.");
+
+
+
+
+
+
+
+
+// /* BMI Calculation */
+// var bmi = function (weight, height ) {
+//   return (703 * weight) / (height * height);
+// };
+//
+// var weight = parseFloat(prompt("Enter your weight in pounds:"));
+// var height = parseFloat(prompt ("Enter your height in inches:"));
+//
+// var result = bmi (weight, height).toFixed(1);
+// alert ((result) + " This number actually means NOTHING");
+//
+// /*Converts Celsius to Fahrenheit*/
+// var tempC = function (num1) {
+//   return (num1 * 1.8) + 32;
+// };
+// var num1 = parseInt(prompt("Enter the temp in Celsius:"))
+// var result = tempC (num1);
+// alert ("That is " + (result) + " degrees Celsius.");
+//
+// /*Converts Fahrenheit to Celsius*/
+// var tempF = function (num1) {
+//   return (num1 - 32) * .5556;
+// };
+// var num1 = parseInt(prompt("Enter the temp in Fahrenheit:"));
+// var result = tempF (num1);
+// alert ("That is " + (result) + " degrees Fahrenheit.");
+//
+// /*Converts Gallons to Liters*/
+// var gallon = function (num1) {
+//   return num1 * 3.78541;
+// };
+//
+// var num1 = parseFloat (prompt("Enter in the number of gallons:"));
+// var result = gallon (num1);
+// alert ((num1) + " gallons is " + (result) + " liters.");
 
 // var str = 'twas the night before Xmas...';
 // var newstr = str.replace (/xmas/i, 'Christmas');
